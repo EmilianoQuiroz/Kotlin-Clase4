7a215838288257cbea4b5b74e1d0561b374f483a
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2022-04-15 22:18:54 -0300

    Funciones

---
+++ src/main.kt
@@ -0,0 +1,44 @@
+fun main() {
+/*
+Temario de la clase 4:
+-Funciones
+-Condicionales
+    * If - “Then” - Else
+-When
+- Operadores de comparación
+- Iteradores
+    * For
+    *While
+    *Do While
+-Repaso de Arrays
+-Taller
+*/
+//---FUNCIONES---//
+/*
+Las funciones son bloques de código que sirven para organizar la
+estructura de nuestro programa.
+Las funciones se definen a partir de la necesidad de ser reutilizadas en
+diferentes partes del código.
+Las funciones pueden devolver un resultado o no. Muchas veces se hace
+la distinción entre Función (cuando devuelve un valor) y Procedimiento
+(Cuando no devuelve nada). En Kotlin llamaremos a los dos tipos,
+simplemente función.
+Por ejemplo:
+Si tengo una función llamada “suma” la voy a invocar cada vez que
+necesite calcular una suma. De esta manera, consigo que el código quede
+mejor articulado y además, es más fácil para mantener.
+*/
+//---ESTRUCTURA DE UNA FUNCION---//
+//    fun nombreFuncion(parametro1:tipo,parametro2:tipo,parametro3:tipo){
+    //La funcion se crea mediante la palabra reservada "fun"
+    //Luego escribimos el nombre de la funcion
+    //Luego pasamos los parametros con su tipo de dato
+    //Puede tener cero, uno, dos o muchos parametros
+    //Por ultimo  entre corchetes escribimos el cuerpo de la funcion
+
+    //...CUERPO DE LA FUNCION...
+
+        //002158
+    }
+
+}
