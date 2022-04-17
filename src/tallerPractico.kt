f36a8f2be5fafe539a077c32d2a2bbfc8d49abb4
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2022-04-17 06:35:40 -0300

    Funciones, condicionales, ciclos, arrays

---
+++ src/tallerPractico.kt
@@ -0,0 +1,34 @@
+//---TALLER PRACTICO---//
+
+//---CICLO FOR---//
+//500000
+
+
+//---EJERCICIOS---//
+/*
+1. Escribir 4 funciones para calcular suma, resta, multiplicación y
+división de dos números guardados en variables predefinidas.
+
+2. Vamos a agregar interactividad a nuestro programa. Para ello, vamos
+a hacer uso de una función que permite el ingreso de datos durante
+la ejecución del programa:
+Agregar a la función anterior la posibilidad de ingresar los dos valores
+en tiempo de ejecución, a través de la consola
+*El programa debe mostrar textos que den información al usuario de lo
+que tiene que hacer y de los resultados, por ejemplo:
+“Programa para sumar”
+“Ingrese el primer número: ”
+“La suma de 2 + 3, dio como resultado 5”
+Dado el siguiente array [12, 0, -4, 48, -52, 25, 2, 31]
+A. Escribir una función que devuelva el resultado de la suma de todos
+sus valores.
+B. Escribir una función que devuelva el promedio de los valores del
+array.
+C. Escribir una función que devuelva la cantidad de números negativos.
+D. Escribir una función que muestre los números impares y además que
+muestre la cantidad.
+E. Escribir una función que muestre los números del Array que sean
+múltiplos de 2, de 3 o de 5
+*Tener en cuenta para todas las operaciones, que el Array pueda
+cambiar, es decir, que es necesario calcular la longitud del Array.
+*/
